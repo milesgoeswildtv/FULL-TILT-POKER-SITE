adec59459f65998f6b0b553e64245be74e3e8fe5
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-12 02:34:01 -0400

    Load flat approved cash felt styles last

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import'./cash-table-v2.css';
 import'./cash-table-v2-polish.css';
 import'./cash-table-v2-polish-v2.css';
 import'./cash-table-approved-layout.css';
+import'./approved-cash-felt.css';
 installSessionRouting();
 const root=createRoot(document.getElementById('root'));
 function render(){const lab=/^#\/layout-lab(?:$|\?)/i.test(location.hash);root.render(lab?<LayoutLab onExit={()=>{location.hash=''}}/>:<><App/><TournamentConfig/><ActionLogPanel/><TableChatDock/></>)}

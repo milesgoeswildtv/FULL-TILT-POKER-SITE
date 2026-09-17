356f517d6283d9a8c2027c460286266850e3bec9
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-17 07:40:44 -0400

    Remove legacy floating gameplay docks from V3

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import{createRoot}from'react-dom/client';
 import App from'./App.jsx';
 import LayoutLab from'./LayoutLab.jsx';
 import TournamentConfig from'./TournamentConfig.jsx';
-import ActionLogPanel from'./ActionLogPanel.jsx';
-import TableChatDock from'./TableChatDock.jsx';
 import{installSessionRouting}from'./session.js';
 import'./fx-runtime.js';
 import'./tournament-config.css';
 installSessionRouting();
 const root=createRoot(document.getElementById('root'));
-function render(){const lab=/^#\/layout-lab(?:$|\?)/i.test(location.hash);root.render(lab?<LayoutLab onExit={()=>{location.hash=''}}/>:<><App/><TournamentConfig/><ActionLogPanel/><TableChatDock/></>)}
+function render(){const lab=/^#\/layout-lab(?:$|\?)/i.test(location.hash);root.render(lab?<LayoutLab onExit={()=>{location.hash=''}}/>:<><App/><TournamentConfig/></>)}
 addEventListener('hashchange',render);
 render();

60f146cc51be1b4f1801e4145b64cd8c2ce79196
milesgoeswildtv <milesgoeswildtv@gmail.com>
2026-09-17 08:28:19 -0400

    Enable V3 pass 2 styling

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ import'./cash-table-approved-layout.css';
 import'./approved-cash-felt.css';
 import'./approved-cash-felt-hotfix.css';
 import'./gameplay-v3-refinement.css';
+import'./gameplay-v3-pass2.css';
 installSessionRouting();
 const root=createRoot(document.getElementById('root'));
 function render(){const preview=/^#\/v3-preview(?:$|\?)/i.test(location.hash),lab=/^#\/layout-lab(?:$|\?)/i.test(location.hash);root.render(preview?<V3Preview/>:lab?<LayoutLab onExit={()=>{location.hash=''}}/>:<><App/><TournamentConfig/><ActionLogPanel/><TableChatDock/></>)}
